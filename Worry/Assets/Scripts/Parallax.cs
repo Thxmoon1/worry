ae646eb24ce0915a5664bc586247054ce7c4c475
rodchenk <rodchenk@th-brandenburg.de>
2019-11-09 16:47:14 +0100

    init commit

---
+++ Worry/Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour{
+
+    public Transform camera;
+    public float speedCoefficient;
+    Vector3 lastpos;
+    // Start is called before the first frame update
+    void Start(){
+        lastpos = camera.position;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        transform.position -= ((lastpos - camera.position) * speedCoefficient);
+        lastpos = camera.position;
+    }
+}
---
+++ Worry/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float speed = 20f;
+    private float horizontalMove = 0f;
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(horizontalMove);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
